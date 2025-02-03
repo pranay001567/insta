cd4860fff812784cd97b3a587a7f4bc7ba76bc75
pranay001567 <wangikarpranay@gmail.com>
2025-02-03 10:12:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('./config/db');
+
+const reelsRoutes = require('./routes/reelsRoutes');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use('/api/reels', reelsRoutes);
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
